28e6629bab06ddc794d7c4353356609224217ec0
Kai Sugahara <git@ksugahara.com>
2024-12-30 12:47:41 +0000

    Add: FM

--- flax_recsys/model/FM.py
+++ flax_recsys/model/FM.py
@@ -4,31 +4,201 @@ from flax import nnx
 
 
 class FM(nnx.Module):
-    """Factorization Machines"""
+    """FM"""
 
-    def __init__(self, data_dim: int, embed_dim: int, rngs: nnx.Rngs):
-        """Initialize model layer and parameters
-
-        Args:
-            data_dim (int): number of dataset dimensions.
-            embed_dim (int): number of feature dimensions.
-            rngs (nnx.Rngs): rng key.
-        """
-
-        self.data_dim = data_dim
-        self.embedder = nnx.Embed(
-            num_embeddings=data_dim, features=embed_dim, rngs=rngs
+    def __init__(
+        self,
+        user_categorical_feature_indices: list[int],
+        user_categorical_feature_cardinalities: list[int],
+        user_numerical_feature_indices: list[int],
+        item_categorical_feature_indices: list[int],
+        item_categorical_feature_cardinalities: list[int],
+        item_numerical_feature_indices: list[int],
+        embed_dim: int,
+        rngs: nnx.Rngs,
+    ):
+        assert len(user_categorical_feature_indices) == len(
+            user_categorical_feature_cardinalities
         )
-        self.linear = nnx.Linear(in_features=data_dim, out_features=1, rngs=rngs)
+        assert len(item_categorical_feature_indices) == len(
+            item_categorical_feature_cardinalities
+        )
+
+        (
+            self.user_categorical_feature_indices,
+            self.user_categorical_feature_cardinalities,
+            self.user_numerical_feature_indices,
+            self.item_categorical_feature_indices,
+            self.item_categorical_feature_cardinalities,
+            self.item_numerical_feature_indices,
+        ) = (
+            user_categorical_feature_indices,
+            user_categorical_feature_cardinalities,
+            user_numerical_feature_indices,
+            item_categorical_feature_indices,
+            item_categorical_feature_cardinalities,
+            item_numerical_feature_indices,
+        )
+
+        # Embedders for Linear Term
+        self.user_lin_cat_embedders = [
+            nnx.Embed(
+                num_embeddings=car,
+                features=1,
+                rngs=rngs,
+            )
+            for car in user_categorical_feature_cardinalities
+        ]
+        self.user_lin_num_embedder = (
+            nnx.Embed(
+                num_embeddings=len(user_numerical_feature_indices),
+                features=1,
+                rngs=rngs,
+            )
+            if len(user_numerical_feature_indices) > 0
+            else None
+        )
+        self.item_lin_cat_embedders = [
+            nnx.Embed(
+                num_embeddings=car,
+                features=1,
+                rngs=rngs,
+            )
+            for car in item_categorical_feature_cardinalities
+        ]
+        self.item_lin_num_embedder = (
+            nnx.Embed(
+                num_embeddings=len(item_numerical_feature_indices),
+                features=1,
+                rngs=rngs,
+            )
+            if len(item_numerical_feature_indices) > 0
+            else None
+        )
+
+        # Embedders for Interaction Term
+        self.user_int_cat_embedders = [
+            nnx.Embed(
+                num_embeddings=car,
+                features=embed_dim,
+                rngs=rngs,
+            )
+            for car in user_categorical_feature_cardinalities
+        ]
+        self.user_int_num_embedder = (
+            nnx.Embed(
+                num_embeddings=len(user_numerical_feature_indices),
+                features=embed_dim,
+                rngs=rngs,
+            )
+            if len(user_numerical_feature_indices) > 0
+            else None
+        )
+        self.item_int_cat_embedders = [
+            nnx.Embed(
+                num_embeddings=car,
+                features=embed_dim,
+                rngs=rngs,
+            )
+            for car in item_categorical_feature_cardinalities
+        ]
+        self.item_int_num_embedder = (
+            nnx.Embed(
+                num_embeddings=len(item_numerical_feature_indices),
+                features=embed_dim,
+                rngs=rngs,
+            )
+            if len(item_numerical_feature_indices) > 0
+            else None
+        )
+
+        # Bias
+        self.bias = nnx.Embed(
+            num_embeddings=1,
+            features=1,
+            rngs=rngs,
+        )
 
     def __call__(self, X: jax.Array) -> jax.Array:
-        # Linear Term
-        linear_term_X = self.linear(X)
+        interaction_term = (
+            jnp.sum(
+                sum(
+                    [
+                        self.user_int_cat_embedders[i](X[:, index])
+                        for i, index in enumerate(self.user_categorical_feature_indices)
+                    ]
+                    + [
+                        self.user_int_num_embedder.embedding[i] * X[:, (index,)]
+                        for i, index in enumerate(self.user_numerical_feature_indices)
+                    ]
+                    + [
+                        self.item_int_cat_embedders[i](X[:, index])
+                        for i, index in enumerate(self.item_categorical_feature_indices)
+                    ]
+                    + [
+                        self.item_int_num_embedder.embedding[i] * X[:, (index,)]
+                        for i, index in enumerate(self.item_numerical_feature_indices)
+                    ]
+                )
+                ** 2,
+                axis=1,
+                keepdims=True,
+            )
+            - sum(
+                [
+                    jnp.sum(
+                        self.user_int_cat_embedders[i](X[:, index]) ** 2,
+                        axis=1,
+                        keepdims=True,
+                    )
+                    for i, index in enumerate(self.user_categorical_feature_indices)
+                ]
+                + [
+                    jnp.sum(
+                        (self.user_int_num_embedder.embedding[i] * X[:, (index,)]) ** 2,
+                        axis=1,
+                        keepdims=True,
+                    )
+                    for i, index in enumerate(self.user_numerical_feature_indices)
+                ]
+                + [
+                    jnp.sum(
+                        self.item_int_cat_embedders[i](X[:, index]) ** 2,
+                        axis=1,
+                        keepdims=True,
+                    )
+                    for i, index in enumerate(self.item_categorical_feature_indices)
+                ]
+                + [
+                    jnp.sum(
+                        (self.item_int_num_embedder.embedding[i] * X[:, (index,)]) ** 2,
+                        axis=1,
+                        keepdims=True,
+                    )
+                    for i, index in enumerate(self.item_numerical_feature_indices)
+                ]
+            )
+        ) / 2
+        bias_term = (
+            sum(
+                [
+                    self.user_lin_cat_embedders[i](X[:, index])
+                    for i, index in enumerate(self.user_categorical_feature_indices)
+                ]
+                + [
+                    self.user_lin_num_embedder.embedding[i]
+                    for i, index in enumerate(self.user_numerical_feature_indices)
+                ]
+                + [
+                    self.item_lin_cat_embedders[i](X[:, index])
+                    for i, index in enumerate(self.item_categorical_feature_indices)
+                ]
+                + [
+                    self.item_lin_num_embedder.embedding[i]
+                    for i, index in enumerate(self.item_numerical_feature_indices)
+                ]
+            )
+            + self.bias.embedding[0]
+        )
 
-        # Interaction Term
-        V = self.embedder.embedding.value
-        interaction_term_X = (
-            jnp.sum((X.dot(V)) ** 2 - (X**2).dot(V**2), axis=1) / 2
-        ).reshape(-1, 1)
-
-        return linear_term_X + interaction_term_X
+        return interaction_term + bias_term
