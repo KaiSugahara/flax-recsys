a9ae21e7ad9c404fdbc0229965007df05cb02925
Kai Sugahara <git@ksugahara.com>
2025-01-04 12:13:06 +0000

    Refactor:

--- flax_recsys/model/FM.py
+++ flax_recsys/model/FM.py
@@ -8,200 +8,91 @@ class FM(nnx.Module):
 
     def __init__(
         self,
-        user_categorical_feature_indices: list[int],
-        user_categorical_feature_cardinalities: list[int],
-        user_numerical_feature_indices: list[int],
-        item_categorical_feature_indices: list[int],
-        item_categorical_feature_cardinalities: list[int],
-        item_numerical_feature_indices: list[int],
+        categorical_feature_cardinalities: list[int],
+        numerical_feature_num: int,
         embed_dim: int,
         rngs: nnx.Rngs,
     ):
-        assert len(user_categorical_feature_indices) == len(
-            user_categorical_feature_cardinalities
-        )
-        assert len(item_categorical_feature_indices) == len(
-            item_categorical_feature_cardinalities
-        )
-
         (
-            self.user_categorical_feature_indices,
-            self.user_categorical_feature_cardinalities,
-            self.user_numerical_feature_indices,
-            self.item_categorical_feature_indices,
-            self.item_categorical_feature_cardinalities,
-            self.item_numerical_feature_indices,
+            self.categorical_feature_cardinalities,
+            self.numerical_feature_num,
         ) = (
-            user_categorical_feature_indices,
-            user_categorical_feature_cardinalities,
-            user_numerical_feature_indices,
-            item_categorical_feature_indices,
-            item_categorical_feature_cardinalities,
-            item_numerical_feature_indices,
+            categorical_feature_cardinalities,
+            numerical_feature_num,
         )
 
-        # Embedders for Linear Term
-        self.user_lin_cat_embedders = [
-            nnx.Embed(
-                num_embeddings=car,
-                features=1,
-                rngs=rngs,
-            )
-            for car in user_categorical_feature_cardinalities
-        ]
-        self.user_lin_num_embedder = (
-            nnx.Embed(
-                num_embeddings=len(user_numerical_feature_indices),
-                features=1,
-                rngs=rngs,
-            )
-            if len(user_numerical_feature_indices) > 0
-            else None
-        )
-        self.item_lin_cat_embedders = [
-            nnx.Embed(
-                num_embeddings=car,
-                features=1,
-                rngs=rngs,
-            )
-            for car in item_categorical_feature_cardinalities
-        ]
-        self.item_lin_num_embedder = (
-            nnx.Embed(
-                num_embeddings=len(item_numerical_feature_indices),
-                features=1,
-                rngs=rngs,
-            )
-            if len(item_numerical_feature_indices) > 0
-            else None
-        )
+        # Coefficient b for Linear Term
+        self.b = {
+            "categorical": {
+                field_i: nnx.Embed(num_embeddings=car, features=1, rngs=rngs)
+                for field_i, car in enumerate(categorical_feature_cardinalities)
+            },
+            "numerical": {
+                field_i: nnx.Embed(num_embeddings=1, features=1, rngs=rngs)
+                for field_i in range(numerical_feature_num)
+            },
+        }
 
-        # Embedders for Interaction Term
-        self.user_int_cat_embedders = [
-            nnx.Embed(
-                num_embeddings=car,
-                features=embed_dim,
-                rngs=rngs,
-            )
-            for car in user_categorical_feature_cardinalities
-        ]
-        self.user_int_num_embedder = (
-            nnx.Embed(
-                num_embeddings=len(user_numerical_feature_indices),
-                features=embed_dim,
-                rngs=rngs,
-            )
-            if len(user_numerical_feature_indices) > 0
-            else None
-        )
-        self.item_int_cat_embedders = [
-            nnx.Embed(
-                num_embeddings=car,
-                features=embed_dim,
-                rngs=rngs,
-            )
-            for car in item_categorical_feature_cardinalities
-        ]
-        self.item_int_num_embedder = (
-            nnx.Embed(
-                num_embeddings=len(item_numerical_feature_indices),
-                features=embed_dim,
-                rngs=rngs,
-            )
-            if len(item_numerical_feature_indices) > 0
-            else None
-        )
+        # Embedding Matrix W for Interaction Term
+        self.W = {
+            "categorical": {
+                field_i: nnx.Embed(num_embeddings=car, features=embed_dim, rngs=rngs)
+                for field_i, car in enumerate(categorical_feature_cardinalities)
+            },
+            "numerical": {
+                field_i: nnx.Embed(num_embeddings=1, features=embed_dim, rngs=rngs)
+                for field_i in range(numerical_feature_num)
+            },
+        }
 
-        # Bias
-        self.bias = nnx.Embed(
-            num_embeddings=1,
-            features=1,
-            rngs=rngs,
-        )
+        # Bias b0
+        self.b0 = nnx.Param(jax.random.normal(rngs.params(), (1,), jnp.float32))
 
-    def __call__(self, X: jax.Array) -> jax.Array:
-        return self.interaction_term(X) + self.bias_term(X)
+    def __call__(self, *Xs: jax.Array) -> jax.Array:
+        categorical_X, numerical_X = Xs
+        return jax.vmap(
+            lambda categorical_X_row, numerical_X_row: (
+                self.bias_term_by_row(categorical_X_row, numerical_X_row)
+                + self.interaction_term_by_row(categorical_X_row, numerical_X_row)
+            ),
+            in_axes=(0),
+            out_axes=(0),
+        )(categorical_X, numerical_X).reshape(-1, 1)
 
-    def interaction_term(self, X: jax.Array) -> jax.Array:
+    def interaction_term_by_row(
+        self, categorical_X_row: jax.Array, numerical_X_row: jax.Array
+    ) -> jax.Array:
+        V = self.generate_V_by_row(categorical_X_row, numerical_X_row)
         return (
-            jnp.sum(
-                sum(
-                    [
-                        self.user_int_cat_embedders[i](X[:, index])
-                        for i, index in enumerate(self.user_categorical_feature_indices)
-                    ]
-                    + [
-                        self.user_int_num_embedder.embedding[i] * X[:, (index,)]
-                        for i, index in enumerate(self.user_numerical_feature_indices)
-                    ]
-                    + [
-                        self.item_int_cat_embedders[i](X[:, index])
-                        for i, index in enumerate(self.item_categorical_feature_indices)
-                    ]
-                    + [
-                        self.item_int_num_embedder.embedding[i] * X[:, (index,)]
-                        for i, index in enumerate(self.item_numerical_feature_indices)
-                    ]
-                )
-                ** 2,
-                axis=1,
-                keepdims=True,
-            )
-            - sum(
-                [
-                    jnp.sum(
-                        self.user_int_cat_embedders[i](X[:, index]) ** 2,
-                        axis=1,
-                        keepdims=True,
-                    )
-                    for i, index in enumerate(self.user_categorical_feature_indices)
-                ]
-                + [
-                    jnp.sum(
-                        (self.user_int_num_embedder.embedding[i] * X[:, (index,)]) ** 2,
-                        axis=1,
-                        keepdims=True,
-                    )
-                    for i, index in enumerate(self.user_numerical_feature_indices)
-                ]
-                + [
-                    jnp.sum(
-                        self.item_int_cat_embedders[i](X[:, index]) ** 2,
-                        axis=1,
-                        keepdims=True,
-                    )
-                    for i, index in enumerate(self.item_categorical_feature_indices)
-                ]
-                + [
-                    jnp.sum(
-                        (self.item_int_num_embedder.embedding[i] * X[:, (index,)]) ** 2,
-                        axis=1,
-                        keepdims=True,
-                    )
-                    for i, index in enumerate(self.item_numerical_feature_indices)
-                ]
-            )
+            (jnp.linalg.norm(V.sum(axis=0)) ** 2)
+            - (jnp.linalg.norm(V, axis=1) ** 2).sum()
         ) / 2
 
-    def bias_term(self, X: jax.Array) -> jax.Array:
-        return (
-            sum(
-                [
-                    self.user_lin_cat_embedders[i](X[:, index])
-                    for i, index in enumerate(self.user_categorical_feature_indices)
-                ]
-                + [
-                    self.user_lin_num_embedder.embedding[i]
-                    for i, index in enumerate(self.user_numerical_feature_indices)
-                ]
-                + [
-                    self.item_lin_cat_embedders[i](X[:, index])
-                    for i, index in enumerate(self.item_categorical_feature_indices)
-                ]
-                + [
-                    self.item_lin_num_embedder.embedding[i]
-                    for i, index in enumerate(self.item_numerical_feature_indices)
-                ]
-            )
-            + self.bias.embedding[0]
+    def bias_term_by_row(
+        self, categorical_X_row: jax.Array, numerical_X_row: jax.Array
+    ) -> jax.Array:
+        return jnp.vstack(
+            [
+                self.b["categorical"][i](value)
+                for i, value in enumerate(categorical_X_row)
+            ]
+            + [
+                self.b["numerical"][i].embedding[0] * value
+                for i, value in enumerate(numerical_X_row)
+            ]
+            + [self.b0.value]
+        ).sum()
+
+    def generate_V_by_row(
+        self, categorical_X_row: jax.Array, numerical_X_row: jax.Array
+    ) -> jax.Array:
+        return jnp.vstack(
+            [
+                self.W["categorical"][i](value)
+                for i, value in enumerate(categorical_X_row)
+            ]
+            + [
+                self.W["numerical"][i].embedding[0] * value
+                for i, value in enumerate(numerical_X_row)
+            ]
         )
