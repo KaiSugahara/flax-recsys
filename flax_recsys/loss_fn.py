299bced31522206ba17baecdc4741c70a0e6019d
Kai Sugahara <git@ksugahara.com>
2025-02-02 13:27:49 +0000

    Add: cross_entropy_loss

---
+++ flax_recsys/loss_fn.py
@@ -0,0 +1,16 @@
+import jax
+import jax.numpy as jnp
+from flax import nnx
+
+
+def cross_entropy_loss(
+    model: nnx.Module, Xs: tuple[jax.Array, ...], y: jax.Array
+) -> jax.Array:
+    # Prediction
+    pred = nnx.softmax(model(*Xs))  # type: ignore
+
+    # Cross Entropy
+    logits = pred[jnp.arange(pred.shape[0]), y.reshape(-1)]
+    loss = -jnp.mean(jnp.log(logits))
+
+    return loss
